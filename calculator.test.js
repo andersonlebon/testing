a4f4f3627096fbc1e6f385b7e9cda58d4ec31457
Caleb Anderson <buyananderson@gmail.com>
2021-07-28 13:30:28 +0200

    Finish testing the mulptiplication

--- calculator.test.js
+++ calculator.test.js
@@ -27,7 +27,7 @@ describe('Substraction', () => {
   it('Temr1 and term2 should be a numbers', () => {
     expect(() => calculator.substract('a', '55')).toThrow();
   });
-  it('Should return the Addition', () => {
+  it('Should return the Substraction', () => {
     const result = calculator.substract(22, 11);
     expect(result).toBe(11);
   });
@@ -43,8 +43,27 @@ describe('Multiplication', () => {
   it('Temr1 and term2 should be a numbers', () => {
     expect(() => calculator.multiply('a', '55')).toThrow();
   });
-  it('Should return the Addition', () => {
+  it('Should return the Mulptiplication', () => {
     const result = calculator.multiply(2, 11);
     expect(result).toBe(22);
+  });
+});
+
+describe('Division', () => {
+  it('Temr1 should be a number', () => {
+    expect(() => calculator.substract('a', 11)).toThrow();
+  });
+  it('Temr2 should be a number', () => {
+    expect(() => calculator.substract(11, 'a')).toThrow();
+  });
+  it('Temr1 and term2 should be a numbers', () => {
+    expect(() => calculator.substract('a', '55')).toThrow();
+  });
+  it('Temr2 should not be 0', () => {
+    expect(() => calculator.substract(1077, 0)).toThrow();
+  });
+  it('Should return the Division', () => {
+    const result = calculator.substract(22, 11);
+    expect(result).toBe(2);
   });
 });
