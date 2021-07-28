bc4426e3116b1d9d4ba174063736b7acabe16363
Caleb Anderson <buyananderson@gmail.com>
2021-07-28 11:43:35 +0200

    Implement the first part of the exercise by adding the second functionnality

--- string.js
+++ string.js
@@ -1,3 +1,6 @@
 module.exports = function stringLength(string) {
+  if (string.length < 1 || string.length >= 10) {
+    throw new error('this condition is invalid');
+  }
   return string.length;
 };
--- string.test.js
+++ string.test.js
@@ -5,3 +5,13 @@ test('Return the length of the string', () => {
   const result = stringLength('hello');
   expect(result).toBe(5);
 });
+
+test('Return the length of the string', () => {
+  expect(() => stringLength('')).toThrow();
+});
+
+test('Return the length of the string', () => {
+  expect(() =>
+    stringLength('Heloo there my name is caleb buyana and i love you all')
+  ).toThrow();
+});
