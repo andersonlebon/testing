e65bba4ebd7cdc75189158bfef9d472fb12b00e9
Caleb Anderson <buyananderson@gmail.com>
2021-07-28 11:49:36 +0200

    Correct the stringLength function

--- string.js
+++ string.js
@@ -1,6 +1,6 @@
 module.exports = function stringLength(string) {
   if (string.length < 1 || string.length >= 10) {
-    throw new error('this condition is invalid');
+    throw new Error('this condition is invalid');
   }
   return string.length;
 };
--- string.test.js
+++ string.test.js
@@ -6,11 +6,11 @@ test('Return the length of the string', () => {
   expect(result).toBe(5);
 });
 
-test('Return the length of the string', () => {
+test('Should throw out an error', () => {
   expect(() => stringLength('')).toThrow();
 });
 
-test('Return the length of the string', () => {
+test('Should throw out an error', () => {
   expect(() =>
     stringLength('Heloo there my name is caleb buyana and i love you all')
   ).toThrow();
