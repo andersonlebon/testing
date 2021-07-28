a2c47b7d8a4b0da92c2dcdbb23d5c5d7df6dd90b
Caleb Anderson <buyananderson@gmail.com>
2021-07-28 13:25:49 +0200

    Finish testing the substraction

--- calculator.test.js
+++ calculator.test.js
@@ -16,3 +16,19 @@ describe('Addition', () => {
     expect(result).toBe(33);
   });
 });
+
+describe('Substraction', () => {
+  it('Temr1 should be a number', () => {
+    expect(() => calculator.substract('a', 11)).toThrow();
+  });
+  it('Temr2 should be a number', () => {
+    expect(() => calculator.substract(11, 'a')).toThrow();
+  });
+  it('Temr1 and term2 should be a numbers', () => {
+    expect(() => calculator.substract('a', '55')).toThrow();
+  });
+  it('Should return the Addition', () => {
+    const result = calculator.substract(22, 11);
+    expect(result).toBe(11);
+  });
+});
